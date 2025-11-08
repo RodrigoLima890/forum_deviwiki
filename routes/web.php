48751d1258feebda5cfccf2c5877345f91caef69
rlima <rlima@gpmsolucoes.com>
2025-11-08 10:49:52 -0300

    create initial project

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TaskController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::prefix('/api/v1')->group(function (){
+    Route::apiResource('/tasks', TaskController::class);
+});
